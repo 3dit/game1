b1dc37591a6f48925a3b9d1d6f15921852d6f11a
Scott Hill <scottmwhill@outlook.com>
2020-07-30 03:22:42 +0000

    updated

--- src/index.js
+++ src/index.js
@@ -9,8 +9,7 @@ const app = new PIXI.Application();
 // can then insert into the DOM
 document.body.appendChild(app.view);
 
-// load the texture we need
-app.loader.add('bunny', 'bunny.png').load((loader, resources) => {
+let myGame = (loader,resources) => {
     // This creates a texture from a 'bunny.png' image
     const bunny = new PIXI.Sprite(resources.bunny.texture);
 
@@ -22,12 +21,42 @@ app.loader.add('bunny', 'bunny.png').load((loader, resources) => {
     bunny.anchor.x = 0.5;
     bunny.anchor.y = 0.5;
 
+    let x = 200;
+    let y = 250;
+    let xv = 2.5;
+    let yv = 1.9;
+    let width = 800;
+    let height = 600;
+
     // Add the bunny to the scene we are building
     app.stage.addChild(bunny);
 
     // Listen for frame updates
-    app.ticker.add(() => {
+    app.ticker.add(function run() {
          // each frame we spin the bunny around a bit
         bunny.rotation += 0.01;
+
+        if(x + xv + bunny.width/2 > width) {
+          xv = -xv;
+        } else if(x + xv  < bunny.width/2) {
+          xv = -xv;
+        }
+        x = x + xv;
+
+        if(y + yv + bunny.height/2 > height) {
+          yv =- yv;
+        } else if(y + yv < bunny.height/2) {
+          yv = -yv;
+        }
+        y += yv;
+
+        bunny.x = x;
+        bunny.y = y;
     });
-});
+
+}
+
+// load the texture we need
+let appLoader = app.loader;
+appLoader.add('bunny', 'test.png');
+appLoader.load(myGame);

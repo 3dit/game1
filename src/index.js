f03ae9b78ddd68f87b9d94a00cae338c2ab8d7ed
xylatoph <xylatoph@mail.google.com>
2020-08-15 17:08:52 -0500

    first commit

--- src/index.js
+++ src/index.js
@@ -1,38 +1,67 @@
 import * as PIXI from 'pixi.js';
 
-// The application will create a renderer using WebGL, if possible,
-// with a fallback to a canvas render. It will also setup the ticker
-// and the root stage PIXI.Container
-const app = new PIXI.Application();
+let app = new PIXI.Application();
+let dude = '';
 
-// The application will create a canvas element for you that you
-// can then insert into the DOM
+
 document.body.appendChild(app.view);
 
-let myGame = (loader,resources) => {
-    // This creates a texture from a 'test.png' image
-    const dude = new PIXI.Sprite(resources.dude.texture);
+document.addEventListener('keydown', doKeyEvent);
+
+
+let myGame = function (loader, resources) {
+    dude = new PIXI.Sprite(resources.dude.texture);
+
 
     // Setup the position of the dude
     dude.x = app.renderer.width / 2;
-    dude.y = app.renderer.height / 2;
+    dude.y = app.renderer.height / 1.2;
 
     // Rotate around the center
     dude.anchor.x = 0.5;
-    dude.anchor.y = 0.5;
+    dude.anchor.y = .5;
 
     // Add the dude to the scene we are building
     app.stage.addChild(dude);
 
     // Listen for frame updates
-    app.ticker.add(function run() {
-         // each frame we spin the dude around a bit
-        dude.rotation += 0.01;
-    });
-
+    app.ticker.add(eventLoop);
 }
 
 // load the texture we need
-let appLoader = app.loader;
-appLoader.add('dude', 'test.png');
-appLoader.load(myGame);
+app.loader.add('dude', 'test.png');
+app.loader.load(myGame);
+
+function eventLoop() {
+    //dude.rotation += .01;
+
+}
+
+let speed = 42;
+
+function doKeyEvent(event) {
+    if (event.key == "d") {
+        dude.x += speed;
+        if (dude.x > app.renderer.width) {
+            dude.x -= app.renderer.width;
+        }
+    }
+    if (event.key == "a") {
+        dude.x -= speed;
+        if (dude.x < 0) {
+            dude.x += app.renderer.width;
+        }
+    }
+    if (event.key == "w") {
+        dude.y -= speed;
+        if (dude.y < 0) {
+            dude.y += app.renderer.height;
+        }
+    }
+    if (event.key == "s") {
+        dude.y += speed;
+        if (dude.y > app.renderer.height) {
+            dude.y -= app.renderer.height;
+        }
+    }
+}
